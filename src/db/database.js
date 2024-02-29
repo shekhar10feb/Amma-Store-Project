71026547d77cd5d2c959293b8b3173343f4c0a55
Shekhar Ranjan <shekhar10feb@gmail.com>
2024-02-29 20:58:30 +0530

    Added files

--- src/db/database.js
+++ src/db/database.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import { app } from "../app.js";
 
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MongoDB is connected!! DB HOST: ${connectionInstance.connection.host}`)
+        app.on("Error", (error) => {
+            console.log("Error in database.js ", error);
+            throw error;
+        })
         // console.log(connectionInstance)
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
